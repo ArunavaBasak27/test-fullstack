0b604e98bffc0b8c86eaf5b1fed19b9846d99ce5
ArunavaBasak27 <arunavadev99@gmail.com>
2025-09-06 16:54:20 +0530

    initial client & server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening at: http://localhost:${PORT}`);
+});
